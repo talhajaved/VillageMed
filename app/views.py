5896481386e6a889949c3f626cf5f5403871cb26
Talha Javed <talhajaved1994@gmail.com>
2015-12-14 14:04:13 -0500

    Added plivoxml

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
+from flask import Response, render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, \
     login_required
 from app import app, db, lm
@@ -6,6 +6,7 @@ from .forms import EditProfileForm, SelectAppointmentForm, AppointmentCompletedF
 from .models import Doctor, Appointment
 from oauth import OAuthSignIn
 import braintree
+import plivoxml
 
 braintree.Configuration.configure(braintree.Environment.Sandbox,
                                 merchant_id="v4pp4mjbfjxmyw3s",
@@ -177,3 +178,53 @@ result = braintree.Transaction.sale({
 #         print("attribute: " + error.attribute)
 #         print("  code: " + error.code)
 #         print("  message: " + error.message)
+
+# Plivo
+# This file will be played when a caller presses 2.
+PLIVO_SONG = "https://s3.amazonaws.com/plivocloud/music.mp3"
+
+# This is the message that Plivo reads when the caller dials in
+IVR_MESSAGE = "Welcome to the Plivo IVR Demo App. Press 1 to hear a random \
+                joke. Press 2 to listen to a song."
+
+# This is the message that Plivo reads when the caller does nothing at all
+NO_INPUT_MESSAGE = "Sorry, I didn't catch that. Please hangup and try again \
+                    later."
+
+# This is the message that Plivo reads when the caller inputs a wrong number.
+WRONG_INPUT_MESSAGE = "Sorry, it's wrong input."
+
+# Enter your own joke handy in case reddit goes down
+PLIVO_JOKE = "What do you get when you cross a snowman with a vampire? \
+            A frostbite"
+
+@app.route('/response/ivr/', methods=['GET', 'POST'])
+def ivr():
+    response = plivoxml.Response()
+    if request.method == 'GET':
+        # GetDigit XML Docs - http://plivo.com/docs/xml/getdigits/
+        getdigits_action_url = url_for('ivr', _external=True)
+        getDigits = plivoxml.GetDigits(action=getdigits_action_url,
+                                       method='POST', timeout=7, numDigits=1,
+                                       retries=1)
+
+        getDigits.addSpeak(IVR_MESSAGE)
+        response.add(getDigits)
+        response.addSpeak(NO_INPUT_MESSAGE)
+
+        return Response(str(response), mimetype='text/xml')
+
+    elif request.method == 'POST':
+        digit = request.form.get('Digits')
+
+        if digit == "1":
+            # Fetch a random joke using the Reddit API.
+            joke = joke_from_reddit()
+            response.addSpeak(joke)
+        elif digit == "2":
+            # Listen to a song
+            response.addPlay(PLIVO_SONG)
+        else:
+            response.addSpeak(WRONG_INPUT_MESSAGE)
+
+        return Response(str(response), mimetype='text/xml')
