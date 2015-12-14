1f73bbccaf260cbc7fcd5ecccc8601146aada2a4
Talha Javed <talhajaved1994@gmail.com>
2015-12-14 14:34:25 -0500

    Updated manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 import os
-from app import create_app, db
+from app import app, db
 from app.models import Doctor, Patient, Appointment, PhoneCalls
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-
 
 def make_shell_context():
     return dict(app=app, db=db, Doctor=Doctor, Patient=Patient, Role=Role,
@@ -17,3 +15,4 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
+
